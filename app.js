e4315bccd7d52bbca6439a39f28e4206d7938003
Maxwell Stern <maxwell.stern@makingwaves.com>
2018-06-20 16:57:23 -0500

    Setup init function:

--- app.js
+++ app.js
@@ -33,12 +33,22 @@ var UIController = (function(){
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
-    var DOM = UIController.getDOMstrings();
+
+    var setupEventListeners = function() {     
+        var DOM = UIController.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(e) {
+            if( e.keyCode === 13 || e.which === 13 ) {
+                ctrlAddItem();
+            }
+        });
+    }
 
     var ctrlAddItem = function() {
         // 1. Get the field input data
         var input = UICtrl.getInput();
-        console.log(input);
 
         // 2. Add item to the budget controller
 
@@ -48,14 +58,14 @@ var controller = (function(budgetCtrl, UICtrl){
 
         // 5. Display the budget on the UI
 
-    }
+    }; 
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(e) {
-        if( e.keyCode === 13 || e.which === 13 ) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            setupEventListeners();
         }
-    })
+    };
 
 })(budgetController, UIController);
+
+controller.init();
