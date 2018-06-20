b78a77fc2a52b85c717ffc8e7dd321b8adb39957
Maxwell Stern <maxwell.stern@makingwaves.com>
2018-06-20 16:28:51 -0500

    Setting up first event listeners.

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
+
+// BUDGET CONTROLLER
 var budgetController = (function() {
 
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b)
-        }
-    }
+    // Some code
 
 })();
 
+// UI CONTROLLER
 var UIController = (function(){
 
+    // Some code
 
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function() {
+        // 1. Get the field input data
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+        // 2. Add item to the budget controller
+
+        // 3. Add item to the UI controller
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
     }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(e) {
+        if( e.keyCode === 13 || e.which === 13 ) {
+            ctrlAddItem();
+        }
+    })
 
 })(budgetController, UIController);
